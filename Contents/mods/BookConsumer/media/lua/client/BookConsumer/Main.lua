533f4b0a1d515b5590a40109e699f6226c367ea2
Konijima <mathieugermain1991@gmail.com>
2022-08-21 17:37:34 -0400

    Compatibility fix Literacy Overhaul

--- Contents/mods/BookConsumer/media/lua/client/BookConsumer/Main.lua
+++ Contents/mods/BookConsumer/media/lua/client/BookConsumer/Main.lua
@@ -10,100 +10,105 @@
 --- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 --- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-local ISReadABook_checkMultiplier = ISReadABook.checkMultiplier
-function ISReadABook:checkMultiplier(...)
-    if SandboxVars.BookConsumer.disabled or not SandboxVars.BookConsumer.consumeLiterature then
-        return ISReadABook_checkMultiplier(self, ...);
-    end
-end
+local function hookISReadABook()
 
-local ISReadABook_start = ISReadABook.start
-function ISReadABook:start()
-    ISReadABook_start(self);
-
-    if not SandboxVars.BookConsumer.disabled then
-        if self.character:getAlreadyReadBook():contains(self.item:getFullType()) then
-            self.character:Say(getText("IGUI_PlayerText_KnowSkill"));
-            self:stop();
-            self:forceStop()
-            return;
-        end
-
-        if SkillBook[self.item:getSkillTrained()] and self.preCompleted then
-            self.character:Say(getText("IGUI_PlayerText_BookObsolete"));
-            self:stop();
-            self:forceStop()
-            return;
+    local ISReadABook_checkMultiplier = ISReadABook.checkMultiplier
+    function ISReadABook:checkMultiplier(...)
+        if SandboxVars.BookConsumer.disabled or not SandboxVars.BookConsumer.consumeLiterature then
+            return ISReadABook_checkMultiplier(self, ...);
         end
     end
-end
 
-local ISReadABook_perform = ISReadABook.perform;
-function ISReadABook:perform()
-    ISReadABook_perform(self);
+    local ISReadABook_start = ISReadABook.start
+    function ISReadABook:start()
+        ISReadABook_start(self);
 
-    -- Add skill level and xp boost
-    if not SandboxVars.BookConsumer.disabled and SandboxVars.BookConsumer.consumeLiterature and SkillBook[self.item:getSkillTrained()] and not self.preCompleted then
-        local trainedStuff = SkillBook[self.item:getSkillTrained()];
+        if not SandboxVars.BookConsumer.disabled then
+            if self.character:getAlreadyReadBook():contains(self.item:getFullType()) then
+                self.character:Say(getText("IGUI_PlayerText_KnowSkill"));
+                self:stop();
+                self:forceStop()
+                return;
+            end
 
-        -- Add skill levels
-        if SandboxVars.BookConsumer.levelPerBook > 0 then
-            --- If book is obsolete do not level up
-            if self.item:getMaxLevelTrained() < self.character:getPerkLevel(SkillBook[self.item:getSkillTrained()].perk) + 1 then
+            if SkillBook[self.item:getSkillTrained()] and self.preCompleted then
                 self.character:Say(getText("IGUI_PlayerText_BookObsolete"));
-            else
-                for i = 1, SandboxVars.BookConsumer.levelPerBook do
-                    self.character:LevelPerk(trainedStuff.perk, false);
-                    self.character:getXp():setXPToLevel(trainedStuff.perk, self.character:getPerkLevel(trainedStuff.perk));
-                    SyncXp(self.character);
+                self:stop();
+                self:forceStop()
+                return;
+            end
+        end
+    end
+
+    local ISReadABook_perform = ISReadABook.perform;
+    function ISReadABook:perform()
+        ISReadABook_perform(self);
+
+        -- Add skill level and xp boost
+        if not SandboxVars.BookConsumer.disabled and SandboxVars.BookConsumer.consumeLiterature and SkillBook[self.item:getSkillTrained()] and not self.preCompleted then
+            local trainedStuff = SkillBook[self.item:getSkillTrained()];
+
+            -- Add skill levels
+            if SandboxVars.BookConsumer.levelPerBook > 0 then
+                --- If book is obsolete do not level up
+                if self.item:getMaxLevelTrained() < self.character:getPerkLevel(SkillBook[self.item:getSkillTrained()].perk) + 1 then
+                    self.character:Say(getText("IGUI_PlayerText_BookObsolete"));
+                else
+                    for i = 1, SandboxVars.BookConsumer.levelPerBook do
+                        self.character:LevelPerk(trainedStuff.perk, false);
+                        self.character:getXp():setXPToLevel(trainedStuff.perk, self.character:getPerkLevel(trainedStuff.perk));
+                        SyncXp(self.character);
+                    end
+                end
+            end
+
+            -- Add skill xp boost
+            if SandboxVars.BookConsumer.xpBoostPerBook > 0 then
+                --- Get the multipler to add
+                local multiplier = (math.floor(SandboxVars.BookConsumer.xpBoostPerBook/10) * (self.maxMultiplier/10));
+
+                --- If book is obsolete reduce multiplier gain by 25%
+                if self.item:getMaxLevelTrained() < self.character:getPerkLevel(SkillBook[self.item:getSkillTrained()].perk) + 1 then
+                    multiplier = multiplier * 0.25;
+                end
+
+                --- Add final multipler
+                if multiplier > self.character:getXp():getMultiplier(trainedStuff.perk) then
+                    self.character:getXp():addXpMultiplier(trainedStuff.perk, multiplier, self.item:getLvlSkillTrained(), self.item:getMaxLevelTrained());
                 end
             end
         end
 
-        -- Add skill xp boost
-        if SandboxVars.BookConsumer.xpBoostPerBook > 0 then
-            --- Get the multipler to add
-            local multiplier = (math.floor(SandboxVars.BookConsumer.xpBoostPerBook/10) * (self.maxMultiplier/10));
-
-            --- If book is obsolete reduce multiplier gain by 25%
-            if self.item:getMaxLevelTrained() < self.character:getPerkLevel(SkillBook[self.item:getSkillTrained()].perk) + 1 then
-                multiplier = multiplier * 0.25;
+        -- Remove any literature if completed
+        if not SandboxVars.BookConsumer.disabled and SandboxVars.BookConsumer.consumeLiterature then
+            if self.item:getReplaceOnUse() then
+                self.item = self.character:getInventory():getItemFromType(self.item:getReplaceOnUse());
             end
-
-            --- Add final multipler
-            if multiplier > self.character:getXp():getMultiplier(trainedStuff.perk) then
-                self.character:getXp():addXpMultiplier(trainedStuff.perk, multiplier, self.item:getLvlSkillTrained(), self.item:getMaxLevelTrained());
-            end
-        end
-    end
-
-    -- Remove any literature if completed
-    if not SandboxVars.BookConsumer.disabled and SandboxVars.BookConsumer.consumeLiterature then
-        if self.item:getReplaceOnUse() then
-            self.item = self.character:getInventory():getItemFromType(self.item:getReplaceOnUse());
-        end
-        if not SkillBook[self.item:getSkillTrained()] or SkillBook[self.item:getSkillTrained()] and not self.preCompleted then
-            self.character:removeFromHands(self.item);
-            if self.item:getContainer() then
-                self.item:getContainer():Remove(self.item);
-            else
-                self.character:getInventory():Remove(self.item);
+            if not SkillBook[self.item:getSkillTrained()] or SkillBook[self.item:getSkillTrained()] and not self.preCompleted then
+                self.character:removeFromHands(self.item);
+                if self.item:getContainer() then
+                    self.item:getContainer():Remove(self.item);
+                else
+                    self.character:getInventory():Remove(self.item);
+                end
             end
         end
     end
+
+    local ISReadABook_new = ISReadABook.new;
+    function ISReadABook:new(character, item, ...)
+        local o = ISReadABook_new(self, character, item, ...);
+        o.stopOnWalk = not SandboxVars.BookConsumer.allowReadWalking; -- allow read and walk
+        if SandboxVars.BookConsumer.disabled then o.stopOnWalk = true; end -- disable read and walk if mod is disabled
+        o.preCompleted = item:getAlreadyReadPages() >= item:getNumberOfPages(); -- get if the book is already readed
+
+        --- Fix instant action for admin/debug
+        if not SandboxVars.BookConsumer.disabled and character:isTimedActionInstant() then
+            o.maxTime = 50;
+        end
+
+        return o;
+    end
+
 end
-
-local ISReadABook_new = ISReadABook.new;
-function ISReadABook:new(character, item, ...)
-    local o = ISReadABook_new(self, character, item, ...);
-    o.stopOnWalk = not SandboxVars.BookConsumer.allowReadWalking; -- allow read and walk
-    if SandboxVars.BookConsumer.disabled then o.stopOnWalk = true; end -- disable read and walk if mod is disabled
-    o.preCompleted = item:getAlreadyReadPages() >= item:getNumberOfPages(); -- get if the book is already readed
-
-    --- Fix instant action for admin/debug
-    if not SandboxVars.BookConsumer.disabled and character:isTimedActionInstant() then
-        o.maxTime = 50;
-    end
-
-    return o;
-end
+Events.OnGameBoot.Add(hookISReadABook);
